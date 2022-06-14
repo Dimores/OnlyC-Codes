34f119d4db58b5639a28d5d589065a55132078a9
Diego Morelo <87763559+Dimores@users.noreply.github.com>
2022-06-14 00:47:32 -0300

    Code optimization

--- ponteiro.cpp
+++ ponteiro.cpp
@@ -1,12 +1,59 @@
 #include <iostream>
 using namespace std;
 
+// Funcao que vai gerar um vetor unidimensional
+int *geraVetor(int n){
+
+    int *vet = new int[n];
+	
+	// Preenchendo o vetor
+	for(int i = 0; i < n; i++){
+		cout << "Informe um numero: ";
+		cin >> vet[i];
+		cout << endl;
+	}
+	
+	return vet;
+	
+}
+
+// Funcao que vai imprimir o vetor unidimensional
+void imprimeVetor(int *vet, int n){
+	
+	cout << "Vetor: \n\n";
+	cout << "[";
+	
+	//Imprimir o valor primeira posicao do vetor
+	cout << vet[0];
+	
+	for(int i = 0; i < n; i++){
+		
+		if(i !=0){
+			cout << ", " << vet[i];
+		}
+	}
+	
+	cout << "]\n";
+	
+}
+
 int main(){
 
-    //ponteiro p para inteiro
-    int *p;
+    /*   -->Variaveis criadas<--
 
-    //tamanho do vetor
+        n = Tamanho do vetor
+        *p = Ponteiro para inteiro(aponta para a primeira posicao do vetor)
+
+
+        -->Funcoes criadas<--
+
+        *geraVetor(int n) = Ponteiro de uma funcao. Atribui a funcao um ponteiro(no caso *p) e gera o vetor com o tamanho n.
+
+        imprimeVetor(int *vet, int n) = Tem como parametro um vetor, em que eu passo um vetor criado(no caso *p), por isso o primeiro parametro tera que ser um vetor,
+        e como segundo parametro o tamanho do vetor(no caso n).
+
+    */
+
     int n = 0;
 
     cout << "\n ------------------->> Programa vetor unidimensional: <<-------------------\n\n";
@@ -15,29 +62,11 @@ int main(){
     cin >> n;
     cout << endl;
 
-    p = new int[n];
-
-    // Preenchendo o vetor
-    for(int i = 0; i < n; i++){
-        cout << "Informe um numero: ";
-        cin >> p[i];
-        cout << endl;
-    }
+    // Criacao do vetor
+    int *p = geraVetor(n);
 
     // Imprimindo o vetor
-    cout << "\nVetor: \n\n";
-
-    cout << "[";
-    cout << p[0];
-
-    for(int i = 0; i < n; i++){
-
-        if(i != 0 ){
-            cout << ", " << p[i];
-        }
-    }
-
-    cout << "]\n";
+    imprimeVetor(p, n);
 
     cout << " -------------------------------------------------------------------------\n";
 
